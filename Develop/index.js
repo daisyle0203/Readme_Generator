f6e3c83b7d4e4ef8d8d66ec6fa58797b5629c4bc
Daisy Le <daisy.le0203@gmail.com>
2022-09-30 19:03:49 -0500

    added needed packages and created runCode function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,62 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require("inquirer")
+const { writeFile } = require("fs").promises
+const { generateMarkdown } = require("./utils")
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const runCode = async () => {
+  try {
+    const answer = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the title of the project?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "What is the project about?",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "What does the user need to install to run this app?",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "What is the use of your project?",
+        name: "usage",
+      },
+      {
+        type: "input",
+        message: "What license is being used?",
+        name: "license",
+        choices: ["None", "Apache 2.0", "MIT", "GPL v3.0"],
+      },
+      {
+        type: "input",
+        message: "Who contributed to this project?",
+        name: "contributions",
+      },
+      {
+        type: "input",
+        message: "What commands are needed to test this app?",
+        name: "tests",
+      },
+      {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+      },
+    ])
+    await writeFile("README.md", generateMarkdown(answer))
+    console.log("Successfully created README.md!")
+  } catch (e) {
+    console.log(e)
+  }
+}
+runCode()
